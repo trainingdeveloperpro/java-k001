5b23c9eb1a287bbdd606e688c1e838fa798a9d9b
Dang Huu Canh <huucanh2092@gmail.com>
2019-07-31 11:19:24 +0700

    Add feature/lesson4-canh

--- src/main/java/com/trainingdeveloperpro/k001/danghuucanh/Main.java
+++ src/main/java/com/trainingdeveloperpro/k001/danghuucanh/Main.java
@@ -1,4 +1,16 @@
 package com.trainingdeveloperpro.k001.danghuucanh;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        HelloWorld helloWorld1 = (HelloWorld) context.getBean("helloWorld1");
+        helloWorld1.print();
+
+        HelloWorld helloWorld2 = (HelloWorld) context.getBean("helloWorld2");
+        helloWorld2.print();
+    }
 }
