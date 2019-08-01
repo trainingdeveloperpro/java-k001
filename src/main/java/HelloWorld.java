a4b6f714f7331fdc182d9d2d59dc4b1107261dff
Do Khuong Duy <dkyfire@gmail.com>
2019-08-01 22:02:32 +0700

    lesson4

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+    private String message;
+
+    public HelloWorld() {
+    }
+
+    public HelloWorld(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void print() {
+        System.out.println("Print: " + this.message);
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        HelloWorld helloWorld1 = (HelloWorld) context.getBean("helloWorld1");
+        helloWorld1.print();
+        HelloWorld helloWorld2 = (HelloWorld) context.getBean("helloWorld2");
+        helloWorld2.print();
+
+    }
+
+}
